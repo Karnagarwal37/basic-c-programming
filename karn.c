f8c77ed3c252ca3c9a16631b57c0e6d072c1d19d
Karnagarwal37 <karnagarwal105491@gmail.com>
2024-08-28 00:02:47 +0530

    Create karn.c

---
+++ karn.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+//print your name
+int main() {
+    printf("karn agarwal\n");
+     printf("karn agarwal\n");
+      printf("karn agarwal\n");
+       printf("karn agarwal\n");
+       int age = 22;
+       printf("age is %d\n",age);
+       float pi = 3.14;
+       printf("pi is %f\n",pi);
+       char star = '*';
+       printf("star is %c\n",star);
+       int ages;
+       printf("enter ages ");
+       scanf("%d",&ages);
+       printf("ages is :%d\n",age);
+       int a,b;
+       printf("enter a\n ");
+       scanf("%d",&a);
+       printf("enter b\n");
+       scanf("%d",&b);
+       int sum= a+b;
+       printf("sum is :%d",sum);
+       
+    return 0;
+}
